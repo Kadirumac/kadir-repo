93526d1294cd7bb00458a48a5786269f46b84a6f
kadirumac <kadirumac49@gmail.com>
2020-04-18 21:24:55 +0200

    ilk calisma projemiz

---
+++ main.js
@@ -0,0 +1,29 @@
+
+var dom = document.querySelector('p').innerText
+
+var dom1 = document.querySelector('p')
+
+console.log(dom);
+
+console.log(dom1);
+
+
+function elma() {
+    alert('welcome');
+
+    console.log('h1 e tikladiniz');
+    
+}
+
+function elmaH1() {
+    alert('h1 basligi');
+    
+    console.log('h1 e tikladiniz');
+    
+}
+function elmaH2() {
+    alert('h2 basligi');
+    
+    console.log('h2 e tikladiniz');
+    
+}
