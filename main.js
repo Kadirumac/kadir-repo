8752cfca38fb3cc431492e9c4f3fa78520fcd909
kadirumac <kadirumac49@gmail.com>
2020-04-21 20:17:28 +0200

    if conditions and DOM

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 
 
 function toplam() {
-
-var x = document.querySelector('#ilk').value
-
-console.log(x);
-
-var y = document.querySelector('.sonraki').value 
-
-console.log( y );
-
-var toplam = Number(x) + Number(y)
+    var x = document.querySelector('#ilk').value
+    console.log(x);
+    var y = document.querySelector('.sonraki').value 
+    console.log( y );    
+    var toplam = Number(x) + Number(y)
+    console.log(toplam);    
+}
 
 
-console.log(toplam);
+var text = 'hello this word comes from js'
+
+document.querySelector('.myText').innerHTML =  text
+console.log(text);
 
 
+
+function baslik() {
+
+
+
+var title = document.querySelector('#head').value
+   
+
+  
+} 
+
+var myMoney = 21321321312
+
+if( myMoney==5){
+   console.log('benim bes eurom var');   
+} else if(myMoney ==10){
+    console.log('benim 10 erurom var zenginim');
+    
+} else{
+    console.log('hic param yok');
     
 }
